a5703e960c3da333aee54469f8d7e8be33442560
Mahendra Kumar <Mahendra.Kumar4@unilever.com>
2023-07-27 14:35:38 +0530

    added new features into the file

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-# main branch - example.py
+# feature_branch - example.py
 
 def main():
-    print("Hello from the main branch!")
+    print("Hello from the feature_branch!")
+
+def new_feature():
+    print("This is a new feature!")
 
 if __name__ == "__main__":
     main()
