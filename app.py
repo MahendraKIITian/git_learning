f8bc32f23e9cff7b5647f386a89e36f9682eb881
Mahendra Kumar <Mahendra.Kumar4@unilever.com>
2023-07-27 14:51:06 +0530

    adding conflicting statement

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 # feature_branch - example.py
 
 def main():
-    print("Hello from the feature_branch!")
+    print("Hello from the modified main branch!")
 
-def new_feature():
-    print("This is a new feature!")
 
 if __name__ == "__main__":
     main()
